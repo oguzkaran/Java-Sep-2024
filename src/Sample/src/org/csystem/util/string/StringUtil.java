f0e2f9b865e5974271d488e5f2db9b43fa92f21f
Oğuz Karan <oguzkaran@csystem.org>
2025-04-19 17:09:24 +0300

    add StringUtil class

--- src/Sample/src/org/csystem/util/string/StringUtil.java
+++ src/Sample/src/org/csystem/util/string/StringUtil.java
@@ -1,5 +1,167 @@
 package org.csystem.util.string;
 
+/**
+ * Uility class for string operations
+ * Last Update: 19th April 2025
+ * @author Java-Sep-2024 Group
+ */
 public class StringUtil {
-	//...
+    public static String capitalize(String s)
+    {
+        return s.isEmpty() ? s : Character.toUpperCase(s.charAt(0)) + s.substring(1).toLowerCase();
+    }
+
+    public static String changeCase(String s)
+    {
+        StringBuilder sb = new StringBuilder(s);
+
+        for (int i = 0; i < sb.length(); ++i) {
+            char c = sb.charAt(i);
+
+            sb.setCharAt(i, Character.isLowerCase(c) ? Character.toUpperCase(c) : Character.toLowerCase(c));
+        }
+
+        return sb.toString();
+    }
+
+    public static int countString(String s1, String s2)
+    {
+        int count = 0;
+
+        for (int i = -1; (i = s1.indexOf(s2, i + 1)) != -1; ++count)
+            ;
+
+        return count;
+    }
+
+    public static String digits(String s)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c))
+                sb.append(c);
+        }
+
+        return sb.toString();
+    }
+
+    public static boolean isPalindrome(String s)
+    {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            char cLeft = s.charAt(left);
+
+            if (!Character.isLetter(cLeft)) {
+                ++left;
+                continue;
+            }
+
+            char cRight = s.charAt(right);
+
+            if (!Character.isLetter(cRight)) {
+                --right;
+                continue;
+            }
+
+            if (Character.toLowerCase(cLeft) != Character.toLowerCase(cRight))
+                return false;
+
+            ++left;
+            --right;
+        }
+
+        return true;
+    }
+
+    public static boolean isPangram(String s, String alphabet)
+    {
+        for (int i = 0; i < alphabet.length(); ++i)
+            if (s.indexOf(alphabet.charAt(i)) == -1)
+                return false;
+
+        return true;
+    }
+
+    public static boolean isPangramEN(String s)
+    {
+        return isPangram(s.toLowerCase(), "abcdefghijklmnopqrstuwxvyz");
+    }
+
+    public static boolean isPangramTR(String s)
+    {
+        return isPangram(s.toLowerCase(), "abcçdefgğhıijklmnoöprsştuüvyz");
+    }
+
+    public static String letters(String s)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+
+            if (Character.isLetter(c))
+                sb.append(c);
+        }
+
+        return sb.toString();
+    }
+
+    public static String padLeading(String s, int n, char ch)
+    {
+        int len = s.length();
+
+        return len < n ? String.valueOf(ch).repeat(n - len) + s : s;
+    }
+
+    public static String padLeading(String s, int n)
+    {
+        return padLeading(s, n, ' ');
+    }
+
+    public static String padTrailing(String s, int n, char ch)
+    {
+        int len = s.length();
+
+        return len < n ? s + String.valueOf(ch).repeat(n - len) : s;
+    }
+
+    public static String padTrailing(String s, int n)
+    {
+        return padTrailing(s, n, ' ');
+    }
+
+    public static String reverse(String s)
+    {
+        return new StringBuilder(s).reverse().toString();
+    }
+
+    public static String trim(String s)
+    {
+        return trimLeading(trimTrailing(s));
+    }
+
+    public static String trimLeading(String s)
+    {
+        int i = 0;
+
+        for (; i < s.length() && Character.isWhitespace(s.charAt(i)); ++i)
+            ;
+
+        return s.substring(i);
+    }
+
+    public static String trimTrailing(String s)
+    {
+        int i = s.length() - 1;
+
+        for (; i >= 0 && Character.isWhitespace(s.charAt(i)); --i)
+            ;
+
+        return s.substring(0, i + 1);
+    }
 }
