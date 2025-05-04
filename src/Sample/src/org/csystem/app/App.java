955d79b2278752de11ae4960149a69dc6760c9aa
Oğuz Karan <oguzkaran@csystem.org>
2025-05-04 20:08:27 +0300

    update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,36 +1,8 @@
 package org.csystem.app;
 
 class App {
-	public static void main(String [] args)
+	public static void main(String[] args)
 	{
-		DigitsTest.run();
+
 	}
 }
-
-class DigitsTest {
-	public static void run()
-	{
-		String s = "Bugün hava 23 derece yarın 25 derece olacakmış";
-		String expected = "2325";
-
-		System.out.println(Util.digits(s).equals(expected));
-	}
-}
-
-class Util {
-	public static String digits(String s)
-	{
-		char [] c = new char[s.length()];
-		int idx = 0;
-
-		for (int i = 0; i < c.length; ++i) {
-			char ch = s.charAt(i);
-
-			if (Character.isDigit(ch))
-				c[idx++] = ch;
-		}
-
-		return new String(c, 0, idx);
-	}
-}
-
