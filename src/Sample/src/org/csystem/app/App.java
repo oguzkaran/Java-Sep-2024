3f21d1780fca610fbe83fd4426f4a56b5a47b8be
Oğuz Karan <oguzkaran@csystem.org>
2025-07-06 20:13:42 +0300

    update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,13 +1,8 @@
 package org.csystem.app;
 
-import org.csystem.util.array.ArrayUtil;
-import org.csystem.util.numeric.NumberUtil;
-
 class App {
 	public static void main(String[] args)
 	{
-		Math m = new Math() //error
-		ArrayUtil au = new ArrayUtil(); //error
-		NumberUtil nu = new NumberUtil(); //error
+
 	}
 }
