341b06ab197287fc8b8dbfb03cd48901ca23af2c
Oğuz Karan <oguzkaran@csystem.org>
2025-07-12 20:09:41 +0300

    update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -15,21 +15,3 @@ class ChemistryUtil {
 	public static final double AVOGADRO_NUMBER = 6.02E22;
 	//...
 }
-
-class Sample {
-	public final int a; //error
-}
-
-class Mample {
-	public final int x; //error
-
-	public Mample()
-	{
-		x = 0;
-	}
-
-	public Mample(int a)
-	{
-
-	}
-}
